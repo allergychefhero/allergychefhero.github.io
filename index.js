2985f1b565cf95e17b9188301c75c8a4e19bce2d
Gabrielle LaBorwit <gabriellelaborwit@gmail.com>
2020-02-21 16:07:04 -0500

    index.js created

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipeList = [
+    {
+      "recipe": "Seafood Pasta",
+      "desc": "",
+      "choices":
+          [
+            "Pasta",
+            "Seafood",
+            "Butter",
+            "Tomatoes"
+          ],
+    },
+    {
+      "recipe": "Pancakes",
+      "desc": "",
+      "choices":
+          [
+            "Flour",
+            "Eggs",
+            "Non-dairy milk",
+            "Syrup"
+          ],
+    }
+];
