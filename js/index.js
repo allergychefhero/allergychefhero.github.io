a7cc48910b2c38372159a85294f6657fbbddcf54
Gabrielle LaBorwit <gabriellelaborwit@gmail.com>
2020-02-23 01:10:22 -0500

    html updated

--- js/index.js
+++ js/index.js
@@ -28,23 +28,27 @@ var recipeList = [
 ];
 
 function startGame() {
-    document.getElementById("title").style.color = "red";
+    // clears elements on page
+    let div = document.getElementById('container');
+    while(div.firstChild){
+      div.removeChild(div.firstChild);
+    }
 }
 
 
 function charSelect(char){
     // if a character is already selected
     if (charSelected){
-        charSelected.style.borderColor = "#00ffff";
         charSelected.getElementsByClassName("char-name")[0].style.display = "none";
+        charSelected.getElementsByClassName("speech-bubble")[0].style.display = "none";
         charSelected.getElementsByClassName("allergen")[0].style.display = "none";
         charSelected.getElementsByClassName("food-desc")[0].style.display = "none";
     }
     charSelected = char;
     char.getElementsByClassName("char-name")[0].style.display = "block";
+    char.getElementsByClassName("speech-bubble")[0].style.display = "block";
     char.getElementsByClassName("allergen")[0].style.display = "block";
     char.getElementsByClassName("food-desc")[0].style.display = "block";
-    char.style.borderColor = "red";
     activateButton();
 }
 
