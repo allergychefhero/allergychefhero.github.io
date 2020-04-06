b70c1b9a08b6e26bb049a54ab6a2268272424c63
Gabrielle LaBorwit <gabriellelaborwit@gmail.com>
2020-04-05 23:59:31 -0400

    starting pages/pick char pages complete

--- js/index.js
+++ js/index.js
@@ -1,32 +1,6 @@
 // globals
     let charSelected;
 
-//Recipes stored in JSON
-var recipeList = [
-    {
-      "recipe": "Seafood Pasta",
-      "desc": "",
-      "choices":
-          [
-            "Pasta",
-            "Seafood",
-            "Butter",
-            "Tomatoes"
-          ],
-    },
-    {
-      "recipe": "Pancakes",
-      "desc": "",
-      "choices":
-          [
-            "Flour",
-            "Eggs",
-            "Non-dairy milk",
-            "Syrup"
-          ],
-    }
-];
-
 function startGame() {
     // clears elements on page
     let div = document.getElementById('container');
@@ -37,20 +11,57 @@ function startGame() {
 
 
 function charSelect(char){
+    //local var declarations
+    let heading = document.getElementById('heading');
+    let speechBubble = document.getElementById('speech-bubble');
+    let foodDesc = document.getElementById('food-desc');
+
     // if a character is already selected
     if (charSelected){
-        charSelected.getElementsByClassName("char-name")[0].style.display = "none";
-        charSelected.getElementsByClassName("speech-bubble")[0].style.display = "none";
-        charSelected.getElementsByClassName("char-img")[0].style.width = "100px";
-        charSelected.getElementsByClassName("allergen")[0].style.display = "none";
-        charSelected.getElementsByClassName("food-desc")[0].style.display = "none";
+        charSelected.style.border="none";
+    //     charSelected.getElementsByClassName("char-name")[0].style.display = "none";
+    //     charSelected.getElementsByClassName("speech-bubble")[0].style.display = "none";
+    //     charSelected.getElementsByClassName("char-img")[0].style.width = "100px";
+    //     charSelected.getElementsByClassName("allergen")[0].style.display = "none";
+    //     charSelected.getElementsByClassName("food-desc")[0].style.display = "none";
+    //
     }
+    foodDesc.style.display = "block";
+
     charSelected = char;
-    char.getElementsByClassName("char-name")[0].style.display = "block";
-    char.getElementsByClassName("speech-bubble")[0].style.display = "block";
-    char.getElementsByClassName("char-img")[0].style.width = "120px";
-    char.getElementsByClassName("allergen")[0].style.display = "block";
-    char.getElementsByClassName("food-desc")[0].style.display = "block";
+    charSelected.style.border = "dashed #333 3px";
+
+    if(charSelected.classList.contains('shelley')){
+        // change background colors
+        speechBubble.style.backgroundColor = "#FFE5BA";
+        foodDesc.style.backgroundColor = "#FFE5BA";
+        // change text
+        heading.innerHTML = "Shelley";
+        speechBubble.innerHTML = "Hi there! Let’s make <strong>seafood pasta!</strong>";
+        foodDesc.innerHTML = "<img src='sprites/seafoodpasta.png' width='100'><br>Shellfish-Free Seafood Pasta";
+    }
+
+    else if(charSelected.classList.contains('nick')){
+        // change background colors
+        speechBubble.style.backgroundColor = "#CEEDCF";
+        foodDesc.style.backgroundColor = "#CEEDCF";
+        // change text
+        heading.innerHTML = "Nick";
+        speechBubble.innerHTML = "Hey! Let’s make an <strong>ice cream sundae</strong>.";
+        foodDesc.innerHTML = "<img src='sprites/sundae.png' width='100'><br>Nut-Free Ice Cream Sundae";
+    }
+
+    else if(charSelected.classList.contains('dani')){
+        // change background colors
+        speechBubble.style.backgroundColor = "#C4ECED";
+        foodDesc.style.backgroundColor = "#C4ECED";
+        // change text
+        heading.innerHTML = "Dani";
+        speechBubble.innerHTML = "We can make <strong>pancakes</strong> together!";
+        foodDesc.innerHTML = "<img src='sprites/pancakes.png' width='100'><br>Dairy-Free Pancakes";
+    }
+
+    //char.getElementsByClassName("food-desc")[0].style.display = "block";
     activateButton();
 }
 
