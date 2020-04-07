07a9bbdc1f456519d1c0f47f8c3abb5453d0a842
Jean Leong <jll034@bucknell.edu>
2020-04-06 23:05:42 -0400

    ingredlist

--- js/ingredlist_TEMPLATE.js
+++ js/ingredlist_TEMPLATE.js
@@ -10,38 +10,46 @@ function startGame() {
 }
 
 
-function selectChoice(choice){
-    const ct = choice.classList.contains("ct");
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
 
-    if(!choiceSelected){
-      choiceSelected = choice;
-
-      // if correct answer selected
-      if(ct){
-        choice.style.backgroundColor = "#CBF7B8";
-        finishQuiz();
-      }
-      //if wrong answer
-      else{
-        choice.style.backgroundColor = "#FFCABA";
-        tryAgain();
-      }
-    }
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
 }
 
-function tryAgain(){
-  document.getElementById('quiz-action-btn').innerHTML="Try Again <big>↺</big>";
+function validate() { 
+	document.getElementById('quiz-action-btn').style.display="none";
+	var checked = 0; 
+	var chks = document.getElementsByTagName("INPUT"); 
+	for (var i = 0; i < chks.length; i++) {
+		if (chks[i].checked) { 
+			checked++; 
+		} 
+	}
+	if(checked==chks.length) { 
+		console.log("WOOPEEE") 
+		nextPage(); 
+	}
 }
 
-function finishQuiz() {
-  document.getElementById('quiz-action-btn').innerHTML="Start! >";
+function nextPage(){
+	document.getElementById('quiz-action-btn').style.display="inline-block";
+  document.getElementById('quiz-action-btn').innerHTML="DONE";
   document.getElementById('quiz-action-btn').style.backgroundColor = "#B9FD9C";
 }
 
+
+
 //Activates start buttons
 function quizProgression() {
-  let btnText = document.getElementById('quiz-action-btn').innerHTML;
-  console.log(btnText);
+  let btnText = document.getElementById('quiz-action-btn').innerHTML; 
   //if quiz completed successfully
   if(btnText == 'Start! &gt;' || btnText == '←'){
     //Clear page contents to prepare for next story page
