97ac8bca404b0cc1a27df86d13f28ff540355584
Gabrielle LaBorwit <gabriellelaborwit@gmail.com>
2020-04-07 19:59:24 -0400

    eliminated need for js/css files for each char, use one central css

--- js/recipequiz.js
+++ js/recipequiz.js
@@ -1,14 +1,10 @@
 // globals
-    let choiceSelected;
+let choiceSelected;
+let ogheading;
 
-function startGame() {
-    // clears elements on page
-    let div = document.getElementById('container');
-    while(div.firstChild){
-      div.removeChild(div.firstChild);
-    }
+function getHeading(){
+    ogheading=document.getElementById('quiz-heading').innerHTML;
 }
-
 
 function selectChoice(choice){
     const ct = choice.classList.contains("ct");
@@ -31,10 +27,14 @@ function selectChoice(choice){
 
 function tryAgain(){
   document.getElementById('quiz-action-btn').style.display = "inline-block";
+  document.getElementById('quiz-heading').innerHTML="I’m allergic to that.";
   document.getElementById('quiz-action-btn').innerHTML="Try Again <big>↺</big>";
 }
 
 function finishQuiz() {
+  document.getElementById('quiz-heading').innerHTML="Great job!";
+  let allergyStatement = document.getElementById('allergy-statement');
+  allergyStatement.parentNode.removeChild(allergyStatement);
   document.getElementById('quiz-action-btn').style.display = "inline-block";
   document.getElementById('quiz-action-btn').innerHTML="Start! >";
   document.getElementById('quiz-action-btn').style.backgroundColor = "#B9FD9C";
@@ -45,7 +45,7 @@ function quizProgression() {
   let btnText = document.getElementById('quiz-action-btn').innerHTML;
   // if next screen button pressed once quiz completed
   if(btnText == 'Start! &gt;'){
-    window.location='ingredlist_CHARNAME.html';
+    window.location='ingredlist_'.concat(document.getElementById('name').className,'.html');
     }
   //if try again-- wrong choice
   else{
@@ -53,6 +53,7 @@ function quizProgression() {
       choiceSelected.style.backgroundColor = "#ffffff";
       choiceSelected = undefined;
       document.getElementById('quiz-action-btn').style.display = "none";
+      document.getElementById('quiz-heading').innerHTML=ogheading;
     }
   }
 }
